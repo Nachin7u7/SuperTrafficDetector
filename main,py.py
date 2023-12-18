60820544107798fdda009a31bfdc675b72bd76b9
Ignacio <ignacio.valencia.sauma@gmail.com>
2023-12-18 15:27:41 -0400

    Ahora se envia imagenes y ademas detecta un video pregrabado

--- main,py.py
+++ main,py.py
@@ -6,8 +6,7 @@ from telegram import Bot
 from telegram.constants import ParseMode
 
 TELEGRAM_BOT_TOKEN = "6338972908:AAF4S04_V5iB9zmTLdwyd94xyWehx9xFyDk"
-TELEGRAM_CHAT_ID = "reemplazar esto por tu chat id"  # <-- chat id de mi cuenta
-
+TELEGRAM_CHAT_ID = "6416110643"  # <-- chat id de mi cuenta
 camera_name = "Calle #8 de obrajes"
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
@@ -18,7 +17,12 @@ with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = net.getUnconnectedOutLayersNames()
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0) # Para usar la camara web
+
+video_path = "coches.mp4"  # Ruta del archivo de video
+
+cap = cv2.VideoCapture(video_path)
+
 
 vehicle_count = 0
 start_time = None
@@ -42,17 +46,21 @@ while True:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.5 and class_id == 2:  # si no me equivoco esto es 'car' xd
+            if confidence > 0.5 and class_id == 2:  # 'car'
                 temp_vehicle_count += 1
 
     if temp_vehicle_count >= 3:
         if start_time is None:
             start_time = time.time()
-        elif time.time() - start_time > 5:  # <---- cambiar el tiempo de detenccion
-            message = "¡Encontramos tráfico en", camera_name, "! tome sus precauciones"
-            asyncio.run(bot.send_message(chat_id=TELEGRAM_CHAT_ID,
-                        text=message, parse_mode=ParseMode.MARKDOWN))
-            break  # <--- error al entrar en loop, no se pueden enviar muchos mensajes
+        elif time.time() - start_time > 5:
+            cv2.imwrite("last_frame.jpg", frame)
+
+            # Enviar mensaje con la imagen
+            message = f"¡Encontramos tráfico en {camera_name}! Tome sus precauciones"
+            with open("last_frame.jpg", "rb") as photo:
+                asyncio.run(bot.send_photo(chat_id=TELEGRAM_CHAT_ID,
+                            photo=photo, caption=message, parse_mode=ParseMode.MARKDOWN))
+            break  # <-- este break nos salva de desastres
 
     else:
         vehicle_count = 0
